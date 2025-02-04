af2378c1807a1f7eec19cdf2e2c5937d8739924e
Mike Pagel <mike@mpagel.de>
2025-02-04 19:08:01 +0100

    add unify dns provider

---
+++ src/mp/deploy_utils/unify/__init__.py
@@ -0,0 +1,5 @@
+# ruff: noqa: PLC0414  # allow exports from __init__.py
+
+from .dns import DnsRecordNotFoundError as DnsRecordNotFoundError
+from .dns import UnifyDnsRecord as UnifyDnsRecord
+from .dns import UnifyDnsRecordProvider as UnifyDnsRecordProvider
